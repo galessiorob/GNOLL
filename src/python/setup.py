cc619e8c8c0a9898d91ffab1ebb57006de693309
Ian Hunter <ian.hunter@intel.com>
2022-12-05 10:55:40 +0000

    first changes

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,25 +1,14 @@
-from platform import uname
-
 from setuptools import setup
+import setuptools.command.build_py
+import subprocess 
+
+class MakeSharedObject(setuptools.command.build_py.build_py):
+  def run(self):
+    subprocess.run(["make", "all"])
 
 
-def in_wsl() -> bool:
-    return "microsoft-standard" in uname().release
-
-
-try:
-    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-
-    class bdist_wheel(_bdist_wheel):
-
-        def finalize_options(self):
-            _bdist_wheel.finalize_options(self)
-            self.root_is_pure = False
-
-except ImportError:
-    bdist_wheel = None
-
-if in_wsl():
-    setup()
-else:
-    setup(cmdclass={"bdist_wheel": bdist_wheel}, )
+setup(
+    cmdclass={
+        'build_py': MakeSharedObject
+    }
+)
